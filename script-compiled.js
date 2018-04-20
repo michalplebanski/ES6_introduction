7863530f5207c86d589bbd3c74d32f026f66470a
Michal <michalplebanski@gmail.com>
2018-04-20 22:49:40 +0200

    fixing bugs

--- script-compiled.js
+++ script-compiled.js
@@ -2,41 +2,58 @@
 
 //ZAD1
 console.log('ZAD1');
+
 var first = 'Hello ';
 var second = 'world!';
 var text = first + ' ' + second;
+
 console.log(text);
 
 //ZAD2
 console.log('ZAD2');
+
 var multiply = function multiply() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-  return console.log('' + a * b);
+	var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+	var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+	return a * b;
 };
-multiply(4, 8);
-multiply(3, 2);
-multiply(9, 6);
-multiply(6);
+
+console.log('' + multiply(4, 8));
+console.log('' + multiply(3, 2));
+console.log('' + multiply(9, 6));
+console.log('' + multiply(6));
 
 // ZAD3
-console.log('ZAD3');
-var sum = 0;
-var length = 0;
-var average = function average() {
-  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-    args[_key] = arguments[_key];
-  }
+console.log('ZAD3 - using forEach');
 
-  args.forEach(function (arg) {
-    return sum += arg;
-  });return sum = sum / args.length;
+var average = function average() {
+	for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+		args[_key] = arguments[_key];
+	}
+
+	var sum = 0;
+	args.forEach(function (arg) {
+		return sum += arg;
+	});
+	return sum / args.length;
 };
-average(2, 4, 6, 8, 1, 3);
-console.log('' + sum);
+
+console.log(average(3, 5, 43, 7));
+console.log(average(9, 3, 4, 9));
+
+//OR
+
+console.log('ZAD3 - using reduce');
+
+var digitals = [1, 5, 8, 9];
+var result = digitals.reduce(function (accumulator, currenValue) {
+	return accumulator + currenValue;
+}) / digitals.length;
+console.log(result);
 
 //ZAD4 
 console.log('ZAD4');
+
 sum = 0;
 var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 var a = grades[0],
@@ -49,13 +66,16 @@ var a = grades[0],
     h = grades[7],
     i = grades[8];
 
+
 console.log(average(a, b, c, d, e, f, g, h, i));
 
 //ZAD5
 console.log('ZAD5');
+
 var array = [1, 4, 'Iwona', false, 'Nowak'];
 var firstItem = array[0],
     lastItem = array[4];
+
 
 console.log(firstItem);
 console.log(lastItem);
--- script.js
+++ script.js
@@ -1,36 +1,60 @@
 //ZAD1
 console.log('ZAD1');
+
 const first = 'Hello ';
 const second = 'world!';
 const text = `${first} ${second}`;
+
 console.log(text);
+
 
 //ZAD2
 console.log('ZAD2');
-const multiply = (a = 1, b = 1) => console.log(`${a*b}`);
-multiply(4,8);
-multiply(3,2);
-multiply(9,6);
-multiply(6);
+
+const multiply = (a = 1, b = 1) => a*b;
+
+console.log(`${multiply(4,8)}`);
+console.log(`${multiply(3,2)}`);
+console.log(`${multiply(9,6)}`);
+console.log(`${multiply(6)}`);
+
 
 // ZAD3
-console.log('ZAD3');
-let sum = 0;
-let length = 0;
-const average = (...args) => {args.forEach(arg => sum += arg); return sum = sum/args.length};
-average(2,4,6,8,1,3);
-console.log(`${sum}`);
+console.log('ZAD3 - using forEach');
+
+const average = (...args) => {
+	let sum = 0;
+	args.forEach(arg => sum += arg);
+	return sum/args.length;
+}
+
+console.log(average(3, 5, 43, 7));
+console.log(average(9, 3, 4, 9));
+
+		//OR
+
+console.log('ZAD3 - using reduce')
+
+const digitals = [1, 5, 8, 9];
+const result = digitals.reduce((accumulator, currenValue) => accumulator + currenValue) / digitals.length;
+console.log(result);
+
+
 
 //ZAD4 
 console.log('ZAD4');
+
 sum = 0;
 const grades = [1,5,5,5,4,3,3,2,1]
-const [a,b,c,d,e,f,g,h,i] = grades;
-console.log(average(a,b,c,d,e,f,g,h,i));
+const [a, b, c, d, e, f, g, h,i] = grades;
+
+console.log(average(a, b, c, d, e, f, g, h, i));
 
 //ZAD5
 console.log('ZAD5');
-const array = [1,4,'Iwona',false,'Nowak'];
-const [firstItem, , , ,lastItem] = array;
+
+const array = [1, 4, 'Iwona', false, 'Nowak'];
+const [firstItem,  ,  ,  , lastItem] = array;
+
 console.log(firstItem);
 console.log(lastItem);
